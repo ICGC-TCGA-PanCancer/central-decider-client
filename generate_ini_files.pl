924974651eb92be7ac697e449ba0119f9f96fe16
Adam Wright <adam.j.wright82@gmail.com>
2015-05-14 13:54:45 -0400

    adding in template for transfering files to s3

--- generate_ini_files.pl
+++ generate_ini_files.pl
@@ -19,7 +19,7 @@ use Data::Dumper;
 
 #USAGE: 
 
-#perl generate_ini_files.pl --workflow-name=SangerPancancerCgpCnIndelSnvStr --gnos-repo=https://gtrepo-ebi.annailabs.com/ --donors=3 --test --template-file=templates/workflow-1.0.5.bsc.ini --password=<pw>
+#perl generate_ini_files.pl --workflow-name=SangerPancancerCgpCnIndelSnvStr --gnos-repo=https://gtrepo-ebi.annailabs.com/ --number-of-donors=3 --test --template-file=templates/workflow-1.0.5.bsc.ini --password=<pw>
 
 #or
 
@@ -45,16 +45,17 @@ $parameters{'vm_location_code'} = 1 if ($ARGV{'--vm_location_code'});
 $parameters{'force'} = 1            if ($ARGV{'--force'});
 $parameters{'test'} = 1             if ($ARGV{'--test'});
 
-if( ($ARGV{'--cloud-env'} && $ARGV{'--whitelist'}) 
-    || ($ARGV{'--donors'} && $ARGV{'--whitelist'})
-    || ($ARGV{'--cloud-env'} && $ARGV{'--donors'})
-    || (!$ARGV{'--cloud-env'} && !$ARGV{'--whitelist'} && !$ARGV{'--donors'})) {
 
-  die "Need to specify only on of the following flags: whitelist, cloud env or donors";
+if( ($ARGV{'--cloud-env'} && $ARGV{'--whitelist'}) 
+    || ($ARGV{'--number-of-donors'} && $ARGV{'--whitelist'})
+    || ($ARGV{'--cloud-env'} && $ARGV{'--number-of-donors'})
+    || (!$ARGV{'--cloud-env'} && !$ARGV{'--whitelist'} && !$ARGV{'--number-of-donors'})) {
+
+  die "Need to specify only on of the following flags: whitelist, cloud env or number-of-donors";
 
 }
-elsif ($ARGV{'--donors'}) {
-    $parameters{'number-of-donors'} = $ARGV{'--donors'}
+elsif ($ARGV{'--number-of-donors'}) {
+    $parameters{'number-of-donors'} = $ARGV{'--number-of-donors'}
 }
 elsif ($ARGV{'--cloud-env'}) {
     $parameters{'cloud-env'} = $ARGV{'--cloud-env'};
@@ -76,10 +77,10 @@ elsif ($ARGV{'--whitelist'}) {
     $parameters{'donor'} = [keys \%donors];
 }
 else {
-  die "Need to specify one of the folling flags: whitelist, cloud env or donors";
+  die "Need to specify one of the folling flags: whitelist, cloud env or number-of-donors";
 } 
 
-my $url = URI->new("http://$host/cgi-bin/central-decider/get-ini");
+my $url = URI->new("http://$host/cgi-bin/feature/central-decider/get-ini");
 $url->query_form(%parameters);
 my $response = $ua->get($url);
 
